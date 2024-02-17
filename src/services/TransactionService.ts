f6d289ad90597b14d8908f8aae5fd601114df428
Aldous Javier <aldous.javier0515@gmail.com>
2024-02-17 15:39:30 +0800

    :sparkles: TransactionService api calls

--- src/services/TransactionService.ts
+++ src/services/TransactionService.ts
@@ -1,4 +1,5 @@
 import axios, { AxiosError } from "axios";
+import { Transaction } from "../types/entities";
 import {
   GroupByDateResp,
   NewTransaction,
@@ -10,42 +11,102 @@ export async function groupByDate(
   month: number,
   token: string
 ): Promise<GroupByDateResp> {
-  return axios
-    .get(`${SERVICE_URL}/groupByDate`, {
+  try {
+    const result = await axios.get(`${SERVICE_URL}/groupByDate`, {
       params: {
         month,
       },
       headers: {
         Authorization: `Bearer ${token}`,
       },
-    })
-    .then((result) => {
-      return result.data;
-    })
-    .catch((error) => {
-      if (error instanceof AxiosError) {
-        return error.response?.data.message;
-      }
-
-      throw error;
     });
+    return result.data;
+  } catch (error) {
+    if (error instanceof AxiosError) {
+      return error.response?.data.message;
+    }
+
+    throw error;
+  }
 }
 
-export async function addTransaction(data: NewTransaction, token: string) {
-  return axios
-    .post(SERVICE_URL, data, {
+export async function addTransaction(
+  data: NewTransaction,
+  token: string
+): Promise<boolean> {
+  try {
+    await axios.post(SERVICE_URL, data, {
       headers: {
         Authorization: `Bearer ${token}`,
       },
-    })
-    .then(() => {
-      return true;
-    })
-    .catch((error) => {
-      if (error instanceof AxiosError) {
-        return error.response?.data.message;
-      }
-
-      throw error;
     });
+    return true;
+  } catch (error) {
+    if (error instanceof AxiosError) {
+      return error.response?.data.message;
+    }
+
+    throw error;
+  }
+}
+
+export async function updateTransaction(
+  data: NewTransaction,
+  token: string,
+  transactionId: number
+): Promise<boolean> {
+  try {
+    await axios.put(`${SERVICE_URL}/${transactionId}`, data, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    return true;
+  } catch (error) {
+    if (error instanceof AxiosError) {
+      return error.response?.data.message;
+    }
+
+    throw error;
+  }
+}
+
+export async function deleteTransaction(
+  token: string,
+  transactionId: number
+): Promise<boolean> {
+  try {
+    await axios.delete(`${SERVICE_URL}/${transactionId}`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    return true;
+  } catch (error) {
+    if (error instanceof AxiosError) {
+      return error.response?.data.message;
+    }
+
+    throw error;
+  }
+}
+
+export async function retrieveTransaction(
+  token: string,
+  transactionId: number
+): Promise<Transaction> {
+  try {
+    const result = await axios.get(`${SERVICE_URL}/${transactionId}`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    return result.data;
+  } catch (error) {
+    if (error instanceof AxiosError) {
+      return error.response?.data.message;
+    }
+
+    throw error;
+  }
 }
